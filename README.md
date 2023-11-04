18ae1522b653089cc9bf103b9f81c4023d314af8
glazk0 <43091603+glazk0@users.noreply.github.com>
2023-11-04 21:34:15 +0100

    feat: error handling with thiserror

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Need to do the README and the rest of the documentation.
 
 Also need to add types/structs for the rest of the API.
 
-Also need to add tests & using thiserror.
+Also need to add tests.
--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,47 +1,14 @@
-use std::error;
-use std::fmt;
+use thiserror::Error;
 
 use crate::client;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum XIVAPIError {
-    /// Occurs when the API has returned a non-success error code.
-    Status(client::Response),
     /// Occurs if the HTTP response from XIVAPI was corrupt and
     /// reqwest could not parse it.
-    Network(client::Error),
+    #[error("An error occurred when processing a request: {0}")]
+    ClientError(#[from] client::Error),
     /// Occurs if serde could not Deserialize the response.
-    Parse(serde_json::Error),
-}
-
-impl From<client::Error> for XIVAPIError {
-    fn from(e: client::Error) -> Self {
-        XIVAPIError::Network(e)
-    }
-}
-
-impl From<serde_json::Error> for XIVAPIError {
-    fn from(e: serde_json::Error) -> Self {
-        XIVAPIError::Parse(e)
-    }
-}
-
-impl fmt::Display for XIVAPIError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            XIVAPIError::Status(ref err) => write!(f, "Status error: {}", err.status()),
-            XIVAPIError::Network(ref err) => err.fmt(f),
-            XIVAPIError::Parse(ref err) => err.fmt(f),
-        }
-    }
-}
-
-impl error::Error for XIVAPIError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            XIVAPIError::Status(_) => None,
-            XIVAPIError::Network(ref err) => Some(err),
-            XIVAPIError::Parse(ref err) => Some(err),
-        }
-    }
+    #[error("An error occurred when parsing a response: {0}")]
+    Parse(#[from] serde_json::Error),
 }
